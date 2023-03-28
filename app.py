2039ca60b5a3bf902a6a3762f1f6edbf606a9a17
Mairieli Wessel <mairieliw@gmail.com>
2023-03-28 19:55:03 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import os
+from flask import Flask, request
+from github import Github, GithubIntegration
+
+app = Flask(__name__)
+
+app_id = '<your_app_number_here>'
+
+# Read the bot certificate
+with open(
+        os.path.normpath(os.path.expanduser('bot_key.pem')),
+        'r'
+) as cert_file:
+    app_key = cert_file.read()
+    
+# Create an GitHub integration instance
+git_integration = GithubIntegration(
+    app_id,
+    app_key,
+)
+
+def pr_opened_event(repo, payload):
+    pr = repo.get_issue(number=payload['pull_request']['number'])
+    author = pr.user.login
+
+    
+    response = f"Thanks for opening this pull request, @{author}! " \
+                f"The repository maintainers will look into it ASAP! :speech_balloon:"
+    pr.create_comment(f"{response}")
+    pr.add_to_labels("needs review")
+
+@app.route("/", methods=['POST'])
+def bot():
+    payload = request.json
+
+    if not 'repository' in payload.keys():
+        return "", 204
+
+    owner = payload['repository']['owner']['login']
+    repo_name = payload['repository']['name']
+
+    git_connection = Github(
+        login_or_token=git_integration.get_access_token(
+            git_integration.get_installation(owner, repo_name).id
+        ).token
+    )
+    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+
+    # Check if the event is a GitHub pull request creation event
+    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
+        pr_opened_event(repo, payload)
+
+    return "", 204
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
